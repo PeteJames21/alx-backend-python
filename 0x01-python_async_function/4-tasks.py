713f315fc2c38fb27750919976d774d3d552cce7
PeteJames21 <peetjames21@gmail.com>
2024-03-18 22:36:29 +0300

    Add module docstring

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
+"""
+Refactor wait_n() to use task_wait_random()
+"""
 import asyncio
-from typing import List
+from typing import Any, Tuple
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> Tuple[Any]:
     """Spawn task_wait_random() n times with the specified max_delay."""
     results = await asyncio.gather(
         *(task_wait_random(max_delay) for _ in range(n))
