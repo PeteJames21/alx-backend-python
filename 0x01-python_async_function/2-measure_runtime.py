1494d87f24b387c70984ce9a0399bb326c7d14fc
PeteJames21 <peetjames21@gmail.com>
2024-03-18 22:18:41 +0300

    Measure the average time taken to run an async task

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+Measure the average time taken to run an async task.
+"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure the average time taken to call wait_n."""
+    t0 = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    t = time.perf_counter() - t0
+    return t / n
