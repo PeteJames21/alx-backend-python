75537d40b2ae0f04452983887b7ff8484145ab0f
PeteJames21 <peetjames21@gmail.com>
2024-03-18 22:37:24 +0300

    Fix return type annnotation

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,12 +3,12 @@
 Refactor wait_n() to use task_wait_random()
 """
 import asyncio
-from typing import Any, Tuple
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> Tuple[Any]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn task_wait_random() n times with the specified max_delay."""
     results = await asyncio.gather(
         *(task_wait_random(max_delay) for _ in range(n))
