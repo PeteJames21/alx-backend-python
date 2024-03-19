052234f9f8a94f91a5ecb710c06d9b409785b28a
PeteJames21 <peetjames21@gmail.com>
2024-03-19 18:01:01 +0300

    Remove unused variable

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,6 +9,6 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """Generate 10 random numbers from 1 to 10."""
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
